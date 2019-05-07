14bd3830e485d9a2c14b1193668ac614185b92fa
Jacky Chen <jingchencmu@gmail.com>
2019-05-07 15:43:56 +0800

    Update length-of-last-word.go

--- Algorithms/0058.length-of-last-word/length-of-last-word.go
+++ Algorithms/0058.length-of-last-word/length-of-last-word.go
@@ -1,17 +1,23 @@
 package problem0058
 
 func lengthOfLastWord(s string) int {
+    strs := strings.Split(strings.TrimSpace(s), " ")
+    return len(strs[len(strs) - 1])
+}
+//
+func lengthOfLastWord(s string) int {
 	size := len(s)
 	if size == 0 {
 		return 0
 	}
+	start := size - 1
+	for ; start >= 0 && s[start] == ' '; start-- {
+	}
+
 	res := 0
-	for i := size - 1; i >= 0; i-- {
+	for i := start; i >= 0; i-- {
 		if s[i] == ' ' {
-			if res != 0 {
-				return res
-			}
-			continue
+			break
 		}
 		res++
 	}
