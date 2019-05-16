81fed704c02395a97f148aa4f8e5f4c56b5c6f22
Jacky Chen <jingchencmu@gmail.com>
2019-05-16 15:07:43 +0800

    Update generate-parentheses.go

--- Algorithms/0022.generate-parentheses/generate-parentheses.go
+++ Algorithms/0022.generate-parentheses/generate-parentheses.go
@@ -1,31 +1,20 @@
-package problem0022
-
 func generateParenthesis(n int) []string {
-	res := make([]string, 0, n*n)
-	bytes := make([]byte, n*2)
-	dfs(n, n, 0, bytes, &res)
-	return res
+    ret := []string{}
+      
+    backtrack(&ret, "", 0, 0, n)
+    return ret
+    
 }
 
-func dfs(left, right, idx int, bytes []byte, res *[]string) {
-	// 所有符号都添加完毕
-	if left == 0 && right == 0 {
-		*res = append(*res, string(bytes))
-		return
-	}
-
-	// "(" 不用担心匹配问题，
-	// 只要 left > 0 就可以直接添加
-	if left > 0 {
-		bytes[idx] = '('
-		dfs(left-1, right, idx+1, bytes, res)
-	}
-
-	// 想要添加 ")" 时
-	// 需要 left < right，
-	// 即在 bytes[:idx] 至少有一个 "(" 可以与 这个 ")" 匹配
-	if right > 0 && left < right {
-		bytes[idx] = ')'
-		dfs(left, right-1, idx+1, bytes, res)
-	}
+func backtrack(ret *[]string, ans string, left, right , n int){
+    if len(ans) == 2*n {
+        *ret = append(*ret, ans)
+        return 
+    }
+    if left < n {
+        backtrack(ret, ans+"(", left+1, right, n)
+    }
+    if right < left {
+        backtrack(ret, ans+")", left, right+1, n)
+    }
 }
