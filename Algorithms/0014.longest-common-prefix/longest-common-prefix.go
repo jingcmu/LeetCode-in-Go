581f049a5a9fd2bdb2ba2abc789d571c9b5eae7a
Jacky Chen <jingchencmu@gmail.com>
2019-05-16 11:49:51 +0800

    Update longest-common-prefix.go

--- Algorithms/0014.longest-common-prefix/longest-common-prefix.go
+++ Algorithms/0014.longest-common-prefix/longest-common-prefix.go
@@ -1,30 +1,25 @@
-package problem0014
-
 func longestCommonPrefix(strs []string) string {
-	short := shortest(strs)
-
-	for i, r := range short {
-		for j := 0; j < len(strs); j++ {
-			if strs[j][i] != byte(r) {
-				return strs[j][:i]
-			}
-		}
-	}
-
-	return short
+    if len(strs) == 0 {
+        return ""
+    }
+    
+    str := strs[0]
+    for i := 1; i < len(strs); i++ {
+        if len(str) < len(strs[i]) {
+            str = helper(str, strs[i])
+        } else {
+            str = helper(strs[i], str)
+        }
+    }
+    return str
 }
 
-func shortest(strs []string) string {
-	if len(strs) == 0 {
-		return ""
-	}
-
-	res := strs[0]
-	for _, s := range strs {
-		if len(res) > len(s) {
-			res = s
-		}
-	}
-
-	return res
+func helper(s1 string, s2 string) string {
+    for i := 0; i < len(s1); i++ {
+        if s1[i] != s2[i] {
+            return s1[:i]
+        }
+    }
+    
+    return s1
 }
