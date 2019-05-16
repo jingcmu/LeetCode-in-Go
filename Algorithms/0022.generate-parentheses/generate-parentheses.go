22cdd946d5be172b56081f6e59b297b5cbb112e6
Jacky Chen <jingchencmu@gmail.com>
2019-05-16 15:10:31 +0800

    Update generate-parentheses.go

--- Algorithms/0022.generate-parentheses/generate-parentheses.go
+++ Algorithms/0022.generate-parentheses/generate-parentheses.go
@@ -6,14 +6,16 @@ func generateParenthesis(n int) []string {
     
 }
 
-func backtrack(ret *[]string, ans string, left, right , n int){
+func backtrack(ret *[]string, ans string, left int, right int, n int){
     if len(ans) == 2*n {
         *ret = append(*ret, ans)
         return 
     }
+    
     if left < n {
         backtrack(ret, ans+"(", left+1, right, n)
     }
+    
     if right < left {
         backtrack(ret, ans+")", left, right+1, n)
     }
